6a6d81c8a9b7c9f9e8708486320e650484348e02
Diego Ramirez <diegomejiasobsu@gmail.com>
2024-10-06 03:09:32 -0500

    add the new configurations to .env files, and comment the security dependencies

--- Library'sProject/src/main/java/com/riwi/library/LibraryApplication.java
+++ Library'sProject/src/main/java/com/riwi/library/LibraryApplication.java
@@ -1,5 +1,6 @@
 package com.riwi.library;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +8,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class LibraryApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LibraryApplication.class, args);
-	}
+		{
+			Dotenv dotenv = Dotenv.configure().load();
+			System.setProperty("SPRING_DATASOURCE_URL", dotenv.get("SPRING_DATASOURCE_URL"));
+			System.setProperty("SPRING_DATASOURCE_USERNAME", dotenv.get("SPRING_DATASOURCE_USERNAME"));
+			System.setProperty("SPRING_DATASOURCE_PASSWORD", dotenv.get("SPRING_DATASOURCE_PASSWORD"));
 
+			SpringApplication.run(LibraryApplication.class, args);
+		}
+	}
 }
