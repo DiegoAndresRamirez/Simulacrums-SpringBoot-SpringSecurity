4475f949175074283bf04338c2989a75ede9f348
Diego Ramirez <diegomejiasobsu@gmail.com>
2024-10-06 01:48:30 -0500

    create the UserController with its interface

--- Library'sProject/src/main/java/com/riwi/library/infrastructure/controllers/UserController.java
+++ Library'sProject/src/main/java/com/riwi/library/infrastructure/controllers/UserController.java
@@ -1,7 +1,7 @@
 package com.riwi.library.infrastructure.controllers;
 
-import com.riwi.library.application.services.CategoryService;
-import com.riwi.library.infrastructure.IModels.ICategoryModel;
+import com.riwi.library.application.services.UserService;
+import com.riwi.library.infrastructure.IModels.IUserModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,17 +10,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1/category")
-public class CategoryController implements ICategoryModel {
+@RequestMapping("/api/v1/user")
+public class UserController implements IUserModel {
 
     @Autowired
-    private CategoryService categoryService;
+    private UserService userService;
 
     @Override
     @PostMapping("/create")
-    public ResponseEntity<?> create(String name, String description) {
+    public ResponseEntity<?> create(String name, String surname, String email, String password, String role) {
         try{
-            return ResponseEntity.status(HttpStatus.CREATED).body(categoryService.create(name, description));
+            return ResponseEntity.status(HttpStatus.CREATED).body(userService.create(name, surname, email, password, role));
         }catch (Exception e) {
             return ResponseEntity.badRequest().body(e.getMessage());
         }
