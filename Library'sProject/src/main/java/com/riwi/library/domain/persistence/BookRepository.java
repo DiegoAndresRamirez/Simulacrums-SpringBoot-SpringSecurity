f770220c96bdf1c088fab345938b58ef265e6eb5
Diego Ramirez <diegomejiasobsu@gmail.com>
2024-10-06 01:43:05 -0500

    create the AutorRepository with its interface

--- Library'sProject/src/main/java/com/riwi/library/domain/persistence/BookRepository.java
+++ Library'sProject/src/main/java/com/riwi/library/domain/persistence/BookRepository.java
@@ -2,6 +2,8 @@ package com.riwi.library.domain.persistence;
 
 import com.riwi.library.domain.models.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface BookRepository extends JpaRepository<Book, Long> {
 }
