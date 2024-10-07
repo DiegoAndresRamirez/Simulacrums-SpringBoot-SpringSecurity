156e5128b18bc744b09d19fe2a232a32f0708c0e
Diego Ramirez <diegomejiasobsu@gmail.com>
2024-10-07 05:29:08 -0500

    create all repositories to my entities

---
+++ EventsProject/src/main/java/com/events/riwi/domain/persistence/VenueRepository.java
@@ -0,0 +1,7 @@
+package com.events.riwi.domain.persistence;
+
+import com.events.riwi.domain.models.Venue;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface VenueRepository extends JpaRepository<Venue, Long> {
+}
