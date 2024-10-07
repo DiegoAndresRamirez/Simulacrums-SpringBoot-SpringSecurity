35aac0e5675b59b6562c1f8cf52afcc209ae8d15
Diego Ramirez <diegomejiasobsu@gmail.com>
2024-10-07 13:24:02 -0500

    create the initial configuratioon to Auditories

--- EventsProject/src/main/java/com/events/riwi/domain/models/Venue.java
+++ EventsProject/src/main/java/com/events/riwi/domain/models/Venue.java
@@ -4,13 +4,18 @@ package com.events.riwi.domain.models;
 import jakarta.persistence.*;
 import lombok.*;
 import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import java.util.Date;
 import java.util.Set;
 
 @Entity
 @Setter
 @Getter
+@EntityListeners(AuditingEntityListener.class)
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
@@ -30,11 +35,18 @@ public class Venue {
     @OneToMany(mappedBy = "venue")
     private Set<VenueOrganizator> venueOrganizators; // Relación con VenueOrganizator
 
-//
-//    @CreatedBy
-//    private String createBy;
-//
-//    @LastModifiedBy
-//    private String lastModifiedBy;
+    @CreatedDate
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdDate;
+
+    @LastModifiedDate
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date lastModifiedDate;
+
+    @CreatedBy
+    private String createdBy;
+
+    @LastModifiedBy
+    private String lastModifiedBy;
 }
 
